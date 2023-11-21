a2a6de76d48f27a564501eba0803a9705d91307c
siméon bossut <112804898+simeon-bossut@users.noreply.github.com>
2023-11-21 13:39:23 +0100

    push

--- METRO/METRO.cpp
+++ METRO/METRO.cpp
@@ -2,11 +2,35 @@
 //
 
 #include "METRO.h"
-
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	string file_name="biblio_nom_stat.txt";
+	/*cout << "Quel est le nom d fichier voulu ? (sans espace svp) \n\n";
+	cin >> file_name; */
+	fstream my_file(file_name);
+	string stat_name;
+	do {
+		Sleep(2);
+		cout << "Voulez-vous ajouter au fichier un nom de station ? \n si oui, entrez le nom à ajouter au fichier si non tapez N \n\n";
+		cin >> stat_name;
+		while(cin.peek()!='\n'){
+			string add;
+			cin >> add;
+			stat_name += " ";
+			stat_name += add;
+		}
+		
+	    modif_string(stat_name,' ','/');
+		if (stat_name != "N")
+		{
+			my_file << stat_name << " ";
+		}
+
+	} while (stat_name != "N");
+	my_file.close();
+
+	System M1(file_name,10);
+	M1.print();
 }
--- METRO/METRO.h
+++ METRO/METRO.h
@@ -4,5 +4,80 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include<windows.h>//sous windows
+//#include<unistd.h>   // for linux s
+
+const int N_STAT = 12;
 
 // TODO: Référencez ici les en-têtes supplémentaires nécessaires à votre programme.
+
+using namespace std;
+
+void modif_string(string text,char swap1,char swap2)
+{
+	for (auto i = text.begin();i != text.end();++i)
+	{
+		if (*i == swap1)
+		{
+			*i=swap2;
+		}
+	}
+}
+
+class Quai
+{
+public:
+	int ligne;
+	Quai(int line):ligne(line){}
+	int nb_pers = 0;
+	void set_nb_pers(int &nb) { nb_pers = nb; }
+	int get_nb_pers() { return nb_pers; }
+};
+
+class Station 
+{
+protected:
+	int id;
+public:
+	string name;
+	Station(int n_id=-1,string n_name="Inconnu") :id(n_id), name(n_name) {}
+	void set_id(int& n_id) { id = n_id; }
+	void set_name(string& n_name) { name=n_name; }
+	string get_name() { return string(name);  }
+	int get_id() { return id; }
+	void print() {
+		cout << "La station numero "<< id<< "a pour nom"<< name<<"\n\n";
+	}
+};
+
+class System
+{
+public:
+	System(string file_name = "Bibliotheque_noms", int size=0) :METRO(size) { //organisé ainsi: Republique/Beaux/arts Lille/Flandres ...
+		ifstream Name_Stat(file_name);
+		for (int i = 0;i < METRO.size();++i)
+		{
+			string Name;
+			if (Name_Stat.peek()!='\n'&&!Name_Stat.eof()) { Name_Stat >> Name; }
+			else { Name = "Inconnu";}
+			modif_string(Name,'/',' ');
+			METRO[i].set_name(Name);
+			METRO[i].set_id(i);
+			//METRO[i].print();
+		}
+	}
+	void print()
+	{
+		cout << "Le metro comprend:\n\n";
+		int a = 6;
+		for (auto i = METRO.begin();i != METRO.end();++i)
+			cout << "La station numero"<< (*i).get_id() << " a pour nom"<< (*i).get_name()<<"\n\n";
+	}
+private:
+	vector<Station> METRO;
+};
+
