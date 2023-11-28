cc964d5ba5ea154eda9f98223a65855e6bb3b0ee
Alexandre Cadilhac <90194301+Ev3st0rm@users.noreply.github.com>
2023-11-28 13:55:38 +0100

    wtf

--- METRO/METRO.cpp
+++ METRO/METRO.cpp
@@ -1,34 +1,142 @@
-﻿#include "METRO.h"
+﻿#include "METRO.hpp"
 
 using namespace std;
 
 int main()
 {
+
+
+
+
+
+
 	string file_name="biblio_nom_stat.txt";
-	/*cout << "Quel est le nom d fichier voulu ? (sans espace svp) \n\n";
-	cin >> file_name; */
-	fstream my_file(file_name,ios::app);
-	string stat_name;
-	do {
-		Sleep(2);
-		cout << "Voulez-vous ajouter au fichier un nom de station ? \n si oui, entrez le nom à ajouter au fichier si non tapez N \n\n";
-		cin >> stat_name;
-		while(cin.peek()!='\n'){
-			string add;
-			cin >> add;
-			stat_name += " ";
-			stat_name += add;
-		}
-		
-	    modif_string(stat_name,' ','/');
-		if (stat_name != "N")
+	///*cout << "Quel est le nom d fichier voulu ? (sans espace svp) \n\n";
+	//cin >> file_name; */
+	//fstream my_file(file_name,ios::app);
+	//string stat_name;
+	//if (1)
+	//{
+	//	do {
+	//		sf::sleep(sf::milliseconds(2000));
+	//		cout << " entrez le nom de la station à ajouter au fichier si non tapez N \n\n";
+	//		cin >> stat_name;
+	//		while (cin.peek() != '\n') {
+	//			string add;
+	//			cin >> add;
+	//			stat_name += " ";
+	//			stat_name += add;
+	//		}
+
+	//		modif_string(stat_name, ' ', '/');
+	//		if (stat_name != "N")
+	//		{
+	//			my_file << stat_name << " ";
+	//		}
+
+	//	} while (stat_name != "N");
+	//	my_file.close();
+	//}
+
+	//Create the main window
+	sf::RenderWindow window(sf::VideoMode(2000, 1200), "SFML window");//,sf::Style::Fullscreen
+
+	string ASSETS = "../../../../METRO/assets/";
+
+	//// Load a sprite to display
+
+	sf::Texture texture_ram;
+	if (!texture_ram.loadFromFile(ASSETS + "rame2.png"))
+		return EXIT_FAILURE;
+
+	sf::Texture texture_stat;
+	if (!texture_stat.loadFromFile(ASSETS + "station.png"))
+		return EXIT_FAILURE;
+	sf::Vector2u stat_size = texture_stat.getSize();
+	cout << "dimensions de la station" << stat_size.x << "," << stat_size.y << endl << endl;
+
+
+	//System M(file_name,10,texture_ram,texture_stat);
+	//M.print();
+
+	int nb_stat = 10;
+
+	//Sprite rame(texture_ram);
+	//rame.setScale(0.5, 0.5);
+	//Vector2f rame_size = rame.getGlobalBounds().getSize();
+	//cout << "dimensions de la rame" << rame_size.x << "," << rame_size.y << endl << endl;
+	//rame.setOrigin(rame_size.x / 2, rame_size.y / 2);
+
+
+	vector<Sprite> LIGNE_STAT;
+	vector<RectangleShape> LIGNE_TRAJET;
+
+	vector<Vector2f> Ligne;
+	float factor_size_stat = 0.25;
+	for (int i = 0;i < nb_stat;++i)
+	{
+		LIGNE_STAT.push_back(Sprite(texture_stat));
+		Ligne.push_back(Vector2f((float)(75 + 180 * i),(float)( 400 + 50 * (i - 4) * cos(2 * i))));
+		//cout << 15.0 + 30 * i << " " << 50.0 + 5 * (i - 3) * (i - 3)<<endl;
+		LIGNE_STAT.back().setOrigin(stat_size.x * factor_size_stat / 2, stat_size.y * factor_size_stat / 2);
+		LIGNE_STAT.back().setPosition(Ligne[i].x, Ligne[i].y);
+		LIGNE_STAT.back().setScale(factor_size_stat, factor_size_stat);
+		if (i > 0)
 		{
-			my_file << stat_name; //g retiré l'espace en fin de mot
+			float norm_vect = sqrt((Ligne[i].x - Ligne[i - 1].x) * (Ligne[i].x - Ligne[i - 1].x) + (Ligne[i].y - Ligne[i - 1].y) * (Ligne[i].y - Ligne[i - 1].y));
+			// cout << norm_vect<<endl;
+			float angle = acos((Ligne[i].x - Ligne[i - 1].x) / norm_vect) * ((Ligne[i].y - Ligne[i - 1].y) > 0 ? 1 : -1);
+			LIGNE_TRAJET.push_back(RectangleShape(Vector2f(norm_vect, 2.f)));
+			LIGNE_TRAJET.back().setPosition(Ligne[i - 1]);
+			LIGNE_TRAJET.back().setRotation((float)(angle * 180 / 3.14));
+			// cout << LIGNE_TRAJET[i-1].getSize().x << "," << LIGNE_TRAJET[i-1].getSize().y << endl;
+		}
+	}
+
+
+
+
+	while (window.isOpen())
+	{
+		//Process events
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// on regarde le type de l'évènement...
+			switch (event.type)
+			{
+				// fenêtre fermée
+			case sf::Event::Closed:
+				window.close();
+				break;
+				// on ne traite pas les autres types d'évènements
+			default:
+				break;
+			}
 		}
 
-	} while (stat_name != "N");
-	my_file.close();
+		sf::sleep(sf::milliseconds(10));
+		// Clear screen
+		window.clear();
 
-	System M1(file_name,10);
-	M1.print();
+		// Draw the sprite
+		for (int i = 0;i < LIGNE_STAT.size();++i)
+		{
+
+			/*if (i != M.LIGNE.size() - 1)
+			{
+				window.draw(LIGNE_TRAJET[i]);
+			}*/
+			window.draw(LIGNE_STAT[i]);
+		}
+		//rame.setPosition(Vector2f(R1.position.x-rame_size.x/2,R1.position.y - rame_size.y / 2));
+		/*rame.setPosition(R1.position);
+		rame.setRotation(R1.angle * 180 / 3.14);
+		window.draw(rame);*/
+		// Update the window
+		window.display();
+
+	}
+
+	return EXIT_SUCCESS;
 }
---
+++ METRO/METRO.hpp
@@ -0,0 +1,191 @@
+﻿#pragma once
+
+#include <thread>
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include <memory>
+#include <set>
+#include <chrono>
+#include <array>
+#include <mutex>
+#include <string>
+#include <vector>
+#include <cmath>
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Main.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System/Vector2.hpp>
+
+using namespace std;
+using namespace chrono_literals;
+using namespace sf;
+
+std::mutex myMutex;
+
+// TODO: Référencez ici les en-têtes supplémentaires nécessaires à votre programme.
+
+using namespace std;
+
+sf::Vector2f calcul_position_stat(int i)
+{
+	return sf::Vector2f((float)(75 + 180 * i), (float)(400 + 50 * (i - 4) * cos(2 * i)));
+}
+
+void modif_string(string&text,char swap1,char swap2)
+{
+	for (auto i = text.begin();i != text.end();++i)
+	{
+		if (*i == swap1)
+		{
+			*i=swap2;
+		}
+	}
+}
+
+
+
+class Station 
+{
+protected:
+	int id;
+	 
+public:
+	string name;
+
+	sf::Sprite stat_sprite;
+	const sf::Vector2f position;
+
+	Station(int n_id = -1, string n_name = "Inconnu", sf::Vector2f n_position = sf::Vector2f(0, 0),sf::Sprite sprite=Sprite()) 
+	:id(n_id), name(n_name), position(n_position),stat_sprite(sprite) {}
+
+	void set_id(int& n_id) { id = n_id; }
+	void set_name(string& n_name) { name=n_name; }
+	string get_name() { return string(name);  }
+	int get_id() { return id; }
+	void print() {
+		cout << "La station numero "<< id+1 << "a pour nom"<< name<<"\n\n";
+	}
+};
+
+class Rame {
+
+private:
+	int id;
+   
+public:
+
+	sf::Sprite ram_sprite;
+
+	vector<int> destination;//Tableau qui contient le nombre de personnes par destination. Exemple: 12 personnes vont descendre à la station 3 destination[3]=12 
+
+	float acceleration;
+
+	sf::Vector2f direction;
+
+	sf::Vector2f position;
+
+	float speed;
+
+	double angle;
+
+	Rame(int n_id = -1,sf::Sprite sprite=Sprite(), float n_speed = 0.0, sf::Vector2f pos = sf::Vector2f(), sf::Vector2f dir = sf::Vector2f(), double st_angle = 0.0, float n_acc = 0.f)
+		:position(pos), direction(dir), id(n_id), speed(n_speed), angle(st_angle),acceleration(n_acc),ram_sprite(sprite) {}
+
+	void move();
+
+	void start_move(const std::vector<sf::Vector2f>& Line);
+
+};
+
+void Rame::start_move(const std::vector<sf::Vector2f>& Line)
+{
+	for (int i = 0;i < Line.size() - 1;++i) //Line.size()-1
+	{
+		direction = Line[i + 1] - Line[i];
+		angle = atan((direction.y / direction.x) * (direction.y * direction.y > 0 ? 1 : -1));
+		cout << "From" << Line[i].x << "," << Line[i].y << " moving to" << Line[i + 1].x << "," << Line[i + 1].y << endl << endl;
+		while ((abs(position.x - Line[i + 1].x) > 1) || (abs(position.y - Line[i + 1].y) > 1))
+		{
+			move();
+			sf::sleep(sf::milliseconds(10));
+		}
+		cout << "arrive en " << position.x << "," << position.y << "Station N" << i + 1 << endl << endl;
+		sf::sleep(sf::milliseconds(100));
+	}
+	cout << "Finished" << endl;
+}
+
+void Rame::move() {
+
+	std::lock_guard<std::mutex> locked(myMutex);
+	if (speed + acceleration >= 0) { speed += acceleration; }
+
+
+	//cout << "Angle d'orientation: " << angle * 180 / 3.14 << endl ;
+	//cout << "Avant a" << position.x << "," << position.y<<"   ";
+	position.x += (float)(speed * cos(angle));
+	position.y += (float)(speed * sin(angle));
+	//cout << "Maintenant à" << position.x << "," << position.y << endl;
+	//sf::sleep(sf::milliseconds(10));
+   // cin >> angle;
+
+}
+
+
+
+class Quai
+{
+public:
+	int ligne;
+	Quai(int line) :ligne(line) {}
+	int nb_pers = 0;
+	void set_nb_pers(int& nb) { nb_pers = nb; }
+	int get_nb_pers() { return nb_pers; }
+};
+
+
+
+
+
+class System
+{
+
+private:
+	
+public:
+
+	vector<Station> LIGNE;
+	vector<Rame> Engins;
+
+
+	System(string file_name = "bibliotheque_nom_stat.txt", int size=0, sf::Texture text_ram= sf::Texture(), sf::Texture text_stat= sf::Texture()) :LIGNE(size) { //organisé ainsi: Republique/Beaux/arts Lille/Flandres ...
+		ifstream Name_Stat(file_name);
+		for (int i = 0;i < LIGNE.size();++i)
+		{
+			string Name;
+			if (Name_Stat.peek()!='\n'&&!Name_Stat.eof()) { Name_Stat >> Name; }
+			else { Name = "Inconnu";}
+			modif_string(Name,'/',' ');
+			LIGNE.push_back(Station(i+1,Name,calcul_position_stat(i),Sprite(text_stat)));
+			//METRO.back().stat_sprite.setOrigin(METRO.back().position.x, METRO.back().position.x);
+			//METRO[i].print();
+		}
+	}
+	
+	void print();
+};
+
+void System::print()
+{
+	cout << "Le metro comprend:\n\n";
+	int a = 6;
+	for (auto i = LIGNE.begin();i != LIGNE.end();++i)
+	{
+		cout << "La station numero" << (*i).get_id() << " a pour nom " << (*i).get_name() << "\n\n";
+	}
+}
