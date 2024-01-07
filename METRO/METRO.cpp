3da8d03271d4a045c876e0afe46cb9e47f5c395a
siméon bossut <112804898+simeon-bossut@users.noreply.github.com>
2024-01-07 22:08:53 +0100

    coed corrigé et commenté

--- METRO/METRO.cpp
+++ METRO/METRO.cpp
@@ -28,47 +28,15 @@ void Gestion_Rames(vector<Rame>&RAMES,int nb_rames, vector<thread>& THREADS,Lign
 
 int main()
 {
-	srand(std::time(nullptr));
-
 	std::mutex mymutex;
-	//Ancien code obsolète mais potentiellement utile si continuation du  projet
 
-	//string file_name = "biblio_nom_stat.txt";
-	//*cout << "Quel est le nom du fichier voulu ? (sans espace svp) \n\n";
-	//cin >> file_name; */
-	//fstream my_file(file_name,ios::app);
-	//string stat_name;
-	//if (1)
-	//{
-	//	do {
-	//		sf::sleep(sf::milliseconds(2000));
-	//		cout << " entrez le nom de la station à ajouter au fichier si non tapez N \n\n";
-	//		cin >> stat_name;
-	//		while (cin.peek() != '\n') {
-	//			string add;
-	//			cin >> add;
-	//			stat_name += " ";
-	//			stat_name += add;
-	//		}
-
-	//		modif_string(stat_name, ' ', '/');
-	//		if (stat_name != "N")
-	//		{
-	//			my_file << stat_name << " ";
-	//		}
-
-	//	} while (stat_name != "N");
-	//	my_file.close();
-	//}
-
-	//INITIALISATION
+	//INITIALISATION DE LA VIDEO ET IMPORTATION DES IMAGES/POLICES 
 	sf::VideoMode desktopMode = sf::VideoMode::getDesktopMode();
 	sf::RenderWindow window(desktopMode, "METRO LILLE SIMULATION", sf::Style::Fullscreen);//,sf::Style::Fullscreen
 	string ASSETS = "../../../../METRO/assets/";
 
 	sf::Font font;
-	if (!font.loadFromFile(ASSETS + "arial.ttf"))
-	{
+	if (!font.loadFromFile(ASSETS + "arial.ttf")){
 		return EXIT_FAILURE;
 	}
 
@@ -81,10 +49,15 @@ int main()
 		return EXIT_FAILURE;
 	sf::Vector2u stat_size = texture_stat.getSize();
 
-	//AFFICHAGE DE LA RAME
 
-	int nb_stat = 10;
+	//NOMBRE DE STATION MAJORE à 10
+	int nb_stat = 10; 
+	//NOMBRE DE RAMES DANS LE CIRCUIT MAJOREE à 9
 	int nb_rames = 6;
+
+
+	//INTITIALISATION DE TOUS LES SPRITES, DE  LEUR TAILLE,ORIGINE ET NOMBRE (LIGNE 63--> LIGNE 237)
+
 	vector<sf::Sprite> sprite_rames;
 	for (int i = 0;i < nb_rames;++i)
 	{
@@ -138,6 +111,7 @@ int main()
 	Vector2f decalage(0, 35); //espace ligne du haut et ligne du bas
 	float factor_size_stat = (float)0.15; //redimension
 
+	//POSITIONS DES STATIONS PAR CHOIX ARBITRAIRE
 	vector<Vector2f> Positions_potentielles(10);
 	Positions_potentielles[0] = Vector2f(30, 500);
 	Positions_potentielles[1] = Vector2f(150, 180);
@@ -152,24 +126,21 @@ int main()
 
 	for (int i = 0; i < nb_stat; ++i)
 	{
-		SPRITE_STATIONS.push_back(sf::Sprite(texture_stat)); //= sprite +1
+		SPRITE_STATIONS.push_back(sf::Sprite(texture_stat)); 
 		SPRITE_STATIONS.back().setColor(sf::Color(209, 157, 25, 255));
-		//STATION.push_back(sf::Vector2f((float)(300 + 130 * i), 500)); //= station +1
-		STATION.push_back(Positions_potentielles[i]); //Formule imaginée pour répartir de manière quelque peu chaotique les stations
+		STATION.push_back(Positions_potentielles[i]); 
 
 		//STATIONS ET QUAIS
 
 		SPRITE_STATIONS.back().setOrigin(stat_size.x / 2, stat_size.y / 2); //place l'origine de l'image en son centre
 		SPRITE_STATIONS.back().setPosition(STATION[i].x, STATION[i].y); //position
 
-		QUAIS_HAUT_IMG.emplace_back(15.f); //quai_haut +1
+		QUAIS_HAUT_IMG.emplace_back(15.f); 
 		QUAIS_HAUT_IMG.back().setOrigin(15, 15); 
-		//QUAIS_HAUT_IMG.back().setFillColor(sf::Color(33, 91, 217,255)); 
 
-		QUAIS_BAS_IMG.emplace_back(15.f); //quai_bas +1
+		QUAIS_BAS_IMG.emplace_back(15.f);
 		QUAIS_BAS_IMG.back().setOrigin(15, 15); 
-		//QUAIS_BAS_IMG.back().setFillColor(sf::Color(33, 91, 217, 255));
-
+		
 		if (i == 0 || i == nb_stat - 1) //si premiere ou derniere station
 		{
 			SPRITE_STATIONS.back().setScale(0, 0); //on fait disparaître la station
@@ -199,11 +170,10 @@ int main()
 					LIGNE_TRAJET_ALLER.push_back(sf::RectangleShape(sf::Vector2f(norm_vect_haut, 2.f)));
 					LIGNE_TRAJET_RETOUR.push_back(sf::RectangleShape(sf::Vector2f(norm_vect_bas, 2.f)));
 
-					SPRITE_STATIONS.back().setScale(factor_size_stat, factor_size_stat); //taille
+					SPRITE_STATIONS.back().setScale(factor_size_stat, factor_size_stat);
 
 					LIGNE_TRAJET_ALLER.back().setPosition(STATION[i-1]);
 
-					//float angle_debut = tan(abs(QUAIS_HAUT_IMG[i].getPosition().y - QUAIS_HAUT_IMG[i - 1].getPosition().y) / abs(QUAIS_HAUT_IMG[i].getPosition().x - QUAIS_HAUT_IMG[i - 1].getPosition().x)) * ((STATION[i].y - STATION[i - 1].y) > 0 ? -1 : 1);
 					float angle_haut = atan((QUAIS_HAUT_IMG[i - 1].getPosition().y - QUAIS_HAUT_IMG[i].getPosition().y) / (QUAIS_HAUT_IMG[i - 1].getPosition().x - QUAIS_HAUT_IMG[i].getPosition().x));
 					float angle_bas = atan((QUAIS_BAS_IMG[i - 1].getPosition().y - QUAIS_BAS_IMG[i].getPosition().y) / (QUAIS_BAS_IMG[i - 1].getPosition().x - QUAIS_BAS_IMG[i].getPosition().x));
 
@@ -218,7 +188,7 @@ int main()
 					float norm_vect_haut = sqrt((QUAIS_HAUT_IMG[i].getPosition().x - QUAIS_HAUT_IMG[i - 1].getPosition().x) * (QUAIS_HAUT_IMG[i].getPosition().x - QUAIS_HAUT_IMG[i - 1].getPosition().x) + (QUAIS_HAUT_IMG[i].getPosition().y - QUAIS_HAUT_IMG[i - 1].getPosition().y) * (QUAIS_HAUT_IMG[i].getPosition().y - QUAIS_HAUT_IMG[i - 1].getPosition().y));
 					float norm_vect_bas = sqrt((QUAIS_BAS_IMG[i].getPosition().x - QUAIS_BAS_IMG[i - 1].getPosition().x) * (QUAIS_BAS_IMG[i].getPosition().x - QUAIS_BAS_IMG[i - 1].getPosition().x) + (QUAIS_BAS_IMG[i].getPosition().y - QUAIS_BAS_IMG[i - 1].getPosition().y) * (QUAIS_BAS_IMG[i].getPosition().y - QUAIS_BAS_IMG[i - 1].getPosition().y));
 
-					LIGNE_TRAJET_ALLER.emplace_back(sf::Vector2f(norm_vect_haut, 2.f));//LIGNE_TRAJET_ALLER.push_back(sf::RectangleShape(sf::Vector2f(norm_vect_haut, 2.f)));
+					LIGNE_TRAJET_ALLER.emplace_back(sf::Vector2f(norm_vect_haut, 2.f));
 					LIGNE_TRAJET_RETOUR.emplace_back(sf::Vector2f(norm_vect_bas, 2.f));
 
 
@@ -235,18 +205,15 @@ int main()
 			}
 			else
 			{
-				SPRITE_STATIONS.back().setScale(factor_size_stat, factor_size_stat); //taille
+				SPRITE_STATIONS.back().setScale(factor_size_stat, factor_size_stat); 
 
 				QUAIS_HAUT_IMG.back().setPosition(STATION[i] - decalage);
 				QUAIS_BAS_IMG.back().setPosition(STATION[i] + decalage);
-
-				// cout << norm_vect << endl;
 
 				float angle = acos((STATION[i].x - STATION[i - 1].x) / norm_vect) * ((STATION[i].y - STATION[i - 1].y) > 0 ? 1 : -1); // à modif en atan
 
 
 				//création des deux lignes séparées
-
 
 				LIGNE_TRAJET_ALLER.emplace_back(sf::Vector2f(norm_vect, 2.f));
 				LIGNE_TRAJET_RETOUR.emplace_back(sf::Vector2f(norm_vect, 2.f));
@@ -262,41 +229,56 @@ int main()
 			LIGNE_TRAJET_RETOUR.back().setFillColor(sf::Color(33, 91, 217, 255));
 		}
 	}
+
+	//FIN INTIALISATION SPRITES 
 	
-	Ligne LIGNE(STATION, decalage);//objet contenant les stations et les quais
+
+
+
+
+
+
+	//CREATION D'UN OBJET CENTRAL DU CODE PERMETTANT LA GESTION, LA MODIFICATION ET L'ECHANGE FACILE DES DONNES DES STATIONS/QUAIS
+	Ligne LIGNE(STATION, decalage);
+
+	//PRECISION DU PARAMETRE RESPONSABLE DU DECALAGE DANS LA POSITION DES STATIONS ET DE LEUR QUAIS
 	LIGNE.decalage = decalage;
+
+	//VECTEUR CONTENANT TOUTES LES RAMES
 	std::vector<Rame> RAMES(nb_rames);
+
+	//VECTEUR DE THREADS, CHACUNE D'ENTRE ELLE EST ASSOCIEE A LA RAME AYANT LE MEME INDICE DANS LE VECTEUR PRECEDENT
 	std::vector<thread> THREADS;
-	bool run = true;//mis à false pour arrêter le code
-	/*double dist_tot = 0.0;
-	for (int i = 0;i < LIGNE.quais.size();++i)
-	{
-		dist_tot += dist(LIGNE[i], LIGNE[i + 1]);
-	}
-	cout << dist_tot << endl;*/
+
+	//VARIABLE PERMETTANT L'ARRET TOTAL DU PROGRAMME. CE DERNIER SE FINIRA ALORS
+	bool run = true;
+
+	//THREADS GERANT L'ARRIVEEE ALEATOIRE DE NOUVEAU PASSAGERS SUR LES QUAIS
 	thread Pass(&Ligne::Gestion_nouv_passager, &LIGNE,ref(run));
 
+	//LE THREAD LE PLUS IMPORTANT QUI VA APPELER TOUS LES THREADS DU "std::vector<Rame> RAMES" QUI VONT EUX-MEMES GERER L'INTEGRALITE DES RAMES
 	thread T(Gestion_Rames, std::ref(RAMES),nb_rames, std::ref(THREADS), std::ref(LIGNE),ref(run));
 	
 
 
+	//THREAD DU main() QUI GERE L'AFFICHAGE
 	while (window.isOpen()) //affichage
 	{
 		//Process events
 		sf::Event event;
 
-		while (window.pollEvent(event)) // conditions de fermeture de la fenêtre
+		while (window.pollEvent(event))
 		{
-			if ((event.type == sf::Event::KeyPressed)&&(26<event.key.code)&& (35 >=event.key.code))//DE 1 à 9
+			if ((event.type == sf::Event::KeyPressed)&&(26<event.key.code)&& (26+nb_rames >=event.key.code))//SI L'INPUT EST ENTRE 1 ET nb_rames, IL S'AGIT D'UN ARRET D'URGENCE
 			{
-				int id_stop = event.key.code - 26;
+				int id_stop = event.key.code - 26;//RECUPERATION ET TRADUCTION DE L4INPUT EN INTEGER
 				
-				if (RAMES[id_stop - 1].emergency_stop == 2)//SI RAME ARRETEE D'URGENCE, REDEMARRAGE MANUEL
+				if (RAMES[id_stop - 1].emergency_stop == 2)//SI RAME ASSOCIEE DEJA ARRETEE D'URGENCE, ON LA REDEMARRAGE MANUEL
 				{
 					cout << "redemarrage manel de la rame numero " << id_stop << endl;
 					RAMES[id_stop - 1].emergency_stop = 0;//REDEMARRAGE MANUEL
 				}
-				else if (RAMES[id_stop - 1].get_id() != -1) {//si la rame a été mise en service
+				else if (RAMES[id_stop - 1].get_id() != -1) {//ON VERIFIE QUE LA RAME EST BIEN SUR LE CIRCUIT
 					cout << "arret force de la rame numero" << id_stop << endl;
 					RAMES[id_stop - 1].emergency_stop = 2;//ARRET D'URGENCE
 				}
@@ -312,10 +294,8 @@ int main()
 		}
 
 		sf::sleep(sf::milliseconds(10)); //mise à jour de l'écran toutes les 10 millisecondes
-		// Clear screen
 		window.clear();
 
-		// Draw the sprite
 		for (int i = 0; i < SPRITE_STATIONS.size(); ++i)
 		{
 			
@@ -353,18 +333,13 @@ int main()
 
 			window.draw(PASSAGERS_RAME[i]);
 
-			/*INFO_RAMES[i].setString("Siuu");
-			INFO_RAMES[i].setPosition(200, 300);
-			window.draw(INFO_RAMES[i]);*/
-
 			if (RAMES[i].get_id() != -1)
 			{
 				INFO_RAMES[i].setString("La rame NUMERO "+to_string(RAMES[i].get_id()) + " a pour position :\n" + to_string(RAMES[i].position.x) + "," + to_string((RAMES[i].position.y)) + "\n"
-					"Elle se situe entre Station numero " + to_string(RAMES[i].precedente_station) + " et station numéro " + to_string(RAMES[i].precedente_station + 1));//AFFICHAGE PASSAGERS DE CHAQUE RAME
-				//std::string AH = INFO_RAMES[i].getString();
-				//cout << INFO_RAMES[i].getLocalBounds().height<<","<< INFO_RAMES[i].getLocalBounds().width << endl;
+					"Elle se situe entre Station numero " + to_string(RAMES[i].precedente_station) + " et station numéro " + to_string(RAMES[i].precedente_station + 1));
+				
 				INFO_RAMES[i].setPosition(window.getSize().x - INFO_RAMES[i].getLocalBounds().width, i * (INFO_RAMES[i].getLocalBounds().height+20));
-				window.draw(INFO_RAMES[i]);//window.getSize().x-INFO_RAMES[i].getLocalBounds().width
+				window.draw(INFO_RAMES[i]);
 			}
 
 		}
--- METRO/METRO.hpp
+++ METRO/METRO.hpp
@@ -27,27 +27,20 @@ using namespace chrono_literals;
 using namespace sf;
 
 
-// TODO: Référencez ici les en-têtes supplémentaires nécessaires à votre programme.
-
 double PI = 3.14159265358979323846;
 
 const Vector2f Position_start(-100, -100);
 
 std::mutex myMutex;
 
-sf::Vector2f calcul_position_stat(int i)
-{
-	return sf::Vector2f((float)(75 + 180 * i), (float)(400 + 50 * (i - 4) * cos(2 * i))); //c'est quoi ce code de merde poto XD
-}
-
-float dist(sf::Vector2f A, sf::Vector2f B)
+float dist(sf::Vector2f A, sf::Vector2f B)//PERMET DE CALCULER LA DISTANCE 2D ENTRE 2 POINTS DU PLAN
 {
 	return sqrt((A.x - B.x) * (A.x - B.x) + (A.y - B.y) * (A.y - B.y));
 }
 
 
 
-class Ligne
+class Ligne//CLASSE PERMETTANT LA GESTION FACILE DES POINTS DE DEPARTS ET D'ARRIVEE DES RAMES AINSI QUE LA GESTION DES PASSAGERS SUR LES QUAIS
 {
 public:
 	std::vector<sf::Vector2f> ligne;
@@ -55,19 +48,17 @@ public:
 	std::vector<unsigned int> quais;
 	const unsigned int max_passager_quai;
 	Ligne(int size=0, Vector2f dec=Vector2f(0,0),const int max_pass_q=120) :ligne(std::vector<sf::Vector2f>(size)), decalage(dec) , quais(std::vector<unsigned int>(2*size-2)),max_passager_quai(max_pass_q) {}
-	Ligne(std::vector<sf::Vector2f> line, Vector2f dec, const int max_pass_q = 120) :ligne(line), decalage(dec), quais(std::vector<unsigned int>(2*line.size()-2)), max_passager_quai(max_pass_q) {}
+	Ligne(std::vector<sf::Vector2f> line, Vector2f dec, const int max_pass_q = 300) :ligne(line), decalage(dec), quais(std::vector<unsigned int>(2*line.size()-2)), max_passager_quai(max_pass_q) {}
 
 
-	void Gestion_nouv_passager(bool&Run)
+	void Gestion_nouv_passager(bool&Run)//AJOUT ALEATOIRE DE PASSAGERS SUR LES QUAIS
 	{
 		std::mutex my_mutex;
 		std::default_random_engine generator;
-		std::uniform_int_distribution<unsigned int> distribution(0,2);
-		//srand(time(NULL));
-		sf::Clock clock;
+		std::uniform_int_distribution<unsigned int> distribution(0,3);
 		while (Run)
 		{
-			sf::sleep(sf::seconds(1));
+			sf::sleep(sf::milliseconds(500));
 			for(int i=1;i<ligne.size()-2;++i){//on ne prend pas le quai 0 et le (fin de ligne1)-1
 				my_mutex.lock();
 				quais[i] = min(quais[i] + distribution(generator), max_passager_quai);
@@ -78,14 +69,10 @@ public:
 				quais[i] = min(quais[i] + distribution(generator), max_passager_quai);
 				my_mutex.unlock();
 			}
-			if (clock.getElapsedTime() >= sf::seconds(5)) {
-				//show_quais();
-				clock.restart();
-			}
 		}
 	}
 
-	void show_quais()
+	void show_quais()//FONCTION UNTILISEE POUR LE DEBUGAGE
 	{
 		std::mutex my_mutex;
 		myMutex.lock();
@@ -98,7 +85,7 @@ public:
 		myMutex.unlock();
 	}
 
-
+	//FONCTION INDISPENSABLE A LA CLASSE PERMETTANT D'ACCEDER A LA POSITION DU IEME POINT D'INTERET P0UR LA RAME,POINT OU ELLE DEVRA S'ARRETER
 	sf::Vector2f operator[](int indice)//idée d'aller de 0 jusqu'à 2*size
 	{
 		indice %= (2 * ligne.size() - 2);
@@ -119,55 +106,14 @@ public:
 };
 
 
-void modif_string(string&text,char swap1,char swap2)
-{
-	for (auto i = text.begin();i != text.end();++i)
-	{
-		if (*i == swap1)
-		{
-			*i=swap2;
-		}
-	}
-}
-
-																//STATION
-
-class Station 
-{
-protected:
-	int id;
-	 
-public:
-	string name;
-
-	sf::Sprite stat_sprite;
-	const sf::Vector2f position;
-
-	Station(int n_id = -1, string n_name = "Inconnu", sf::Vector2f n_position = sf::Vector2f(0, 0),sf::Sprite sprite = sf::Sprite()) 
-	:id(n_id), name(n_name), position(n_position),stat_sprite(sprite) {}
-
-	void set_id(int& n_id) { id = n_id; }
-	void set_name(string& n_name) { name=n_name; }
-	string get_name() { return string(name);  }
-	int get_id() { return id; }
-	void print() {
-		std::cout << "La station numero "<< id+1 << "a pour nom"<< name<<"\n\n";
-	}
-};
-
-
 
 																//RAME
-
-int capa_max_rame = 200; //à modifier si besoin
 
 
 class Rame {
 
 private:
 	int id;
-	int nb_personnes_dans_rame = 0;
-	int nb_descente = 0;
 
 public:
 
@@ -175,9 +121,9 @@ public:
 
 	vector<int> destination;//Tableau qui contient le nombre de personnes par destination. Exemple: 12 personnes vont descendre à la station 3 destination[3]=12 
 
-	float acceleration;
+	float acceleration;//constante
 
-	sf::Vector2f direction;
+	sf::Vector2f direction;//Vecteur unité qui porte le mouvement de la rame
 
 	sf::Vector2f position;
 
@@ -193,10 +139,10 @@ public:
 
 	unsigned int passagers;
 
-	int emergency_stop;//0 si pas d'arrêt, 1 si arrêt provoqué par la rame suivante , 2 si arrêt mannuel d'urgence
+	int emergency_stop;//0 si pas d'arrêt, 1 si arrêt provoqué par la rame suivante(distance trop courte) , 2 si arrêt mannuel d'urgence
 
-	Rame(int n_id = -1, sf::Vector2f pos = Position_start, sf::Vector2f dir = sf::Vector2f(), double st_angle = 0.0, float n_acc = 15.f, float max_vit = 80, int pre_stat = 0,int pass=0)
-		:position(pos), direction(dir), id(n_id), speed(0.0), angle(st_angle), acceleration(n_acc), max_speed(max_vit), precedente_station(pre_stat),passagers(pass),max_passagers(80),emergency_stop(0){}
+	Rame(int n_id = -1, sf::Vector2f pos = Position_start, sf::Vector2f dir = sf::Vector2f(), double st_angle = 0.0, float n_acc = 15.f, float max_vit = 120, int pre_stat = 0,int pass=0)
+		:position(pos), direction(dir), id(n_id), speed(0.0), angle(st_angle), acceleration(n_acc), max_speed(max_vit), precedente_station(pre_stat),passagers(pass),max_passagers(200),emergency_stop(0){}
 
 	void move(sf::Time wait_time);
 
@@ -256,21 +202,23 @@ void Rame::start_move( std::vector<Rame>& RAMES,Ligne & LIGNE, bool& Run)
 		if (emergency_stop == 0)
 		{
 			int i = precedente_station;
-			if (position == LIGNE[precedente_station])
+			if (position == LIGNE[precedente_station])//SI LA RAME EST BIEN ARRIVEE A DESTINATION
 			{
-				if (i + 1 % (LIGNE.ligne.size() - 1) != 0)
+				sf::sleep(sf::milliseconds(200));
+				if (i + 1 % (LIGNE.ligne.size() - 1) != 0)// SI LA STATION N'EST PAS AUX EXTREMITES (FAUSSES STATIONS)
 				{
 					int descending_passengers = passagers * ((float)1 / (LIGNE.ligne.size() - 1 - i % (LIGNE.ligne.size() - 1)));
 					passagers *= (1 - ((float)1 / (LIGNE.ligne.size() - 1 - i % (LIGNE.ligne.size() - 1))));
 					sf::sleep(sf::milliseconds(15 * descending_passengers));
 				}
-
+				sf::sleep(sf::milliseconds(200));
 				int incoming_passengers = min(max_passagers - passagers, LIGNE.quais[i]);//le min entre la place disponible
 				passagers += incoming_passengers;
 				LIGNE.quais[i] -= incoming_passengers;
 				sf::sleep(sf::milliseconds(15 * incoming_passengers));
 			}
 
+			int dist_lim = 350;//distance minimale à avoir entre 2 rames. Si la distance passse ce seuil, la RAME SUIVANTE ralentit et s'arrête d'URGENCE. ELLE REDEMARERA UNE FOIS UNE CERTAINE DISTANCE AVEC LA PROCHAINE ATTEINTE
 			int flag = -1;
 			Vector2f PointOrig = position;
 			Vector2f PointDest = LIGNE[i + 1];
@@ -282,28 +230,24 @@ void Rame::start_move( std::vector<Rame>& RAMES,Ligne & LIGNE, bool& Run)
 			double dist_freinage = min(abs(max_speed * max_speed / (2 * acceleration)), dist(PointOrig, PointDest) / 2);
 
 			float epsilon = 3;
-			//sf::Clock clock;
-			sf::Time counter(sf::seconds(0));
+			sf::Time counter(sf::seconds(0));//Pas utilisz actuellement mais permet de savoir le temps théoriquement passé si les instructions étaient instantanees
+			sf::Time wait_time = sf::milliseconds(15);
 				
-			while ((dist(position, PointDest) > epsilon)&&((speed!=0.0)||(emergency_stop==0)))//((abs(position.x - PointDest.x) > epsilon) || (abs(position.y - PointDest.y) > epsilon))
+			while ((dist(position, PointDest) > epsilon)&&((speed!=0.0)||(emergency_stop==0)))//SI LA RAME EST TRES PROCHE DE SA DESTINATION OU QUE L'ARRET D'URGENCE(MANUEL OU AUTOMATIQUE) EST ACTIVE, ON QUITTE LA BOUCLE
 			{
-				sf::Time wait_time = sf::milliseconds(15);//calcul du temps la période de refresh, maxée à 125ms donc 8px/s 
-				double Dist_next;
-
-
-				Dist_next = dist_to_next(RAMES, LIGNE);
+				double Dist_next = dist_to_next(RAMES, LIGNE);
 				//if (get_id() == 3) { cout << Dist_next << endl; }
 
-				if ((flag == -1) && (((Dist_next < 300) && (Dist_next >= 0)) || (abs(dist(position, PointDest) - dist_freinage) < 0.5) || (emergency_stop != 0)))
-				{
-					if ((Dist_next < 300) && (Dist_next >= 0))
+				if ((flag == -1) && (((Dist_next < dist_lim) && (Dist_next >= 0)) || (abs(dist(position, PointDest) - dist_freinage) < 0.5) || (emergency_stop != 0)))//VERIFICATION DE LA DISTANCE ENTRE LA RAME ET CELLE QUI LA PRECEDE
+				{																																				//VERIFICATION DU MOMENT OU COMMENCER A FREINER
+					if ((Dist_next < dist_lim) && (Dist_next >= 0))
 					{
 						emergency_stop = 1;
 					}
-					acceleration *= flag;
+					acceleration *= flag;//INVERSATION DE L'ACCELERATION(FREINAGE)
 					flag = 1;
 				}
-				if (speed + acceleration * wait_time.asSeconds() >= 0) {
+				if (speed + acceleration * wait_time.asSeconds() >= 0) {//VARIATION DE LA VITESSE
 					if (speed + acceleration * wait_time.asSeconds() >= max_speed) {
 						speed = max_speed;
 					}
@@ -319,7 +263,6 @@ void Rame::start_move( std::vector<Rame>& RAMES,Ligne & LIGNE, bool& Run)
 				sf::sleep(wait_time);
 			}
 
-			//std::cout <<"La rame "<< id << "arrive en " << position.x << "," << position.y << "Station N" << i + 1 << endl;
 			acceleration = abs(acceleration);
 			speed = 0.0;
 			if (emergency_stop == 0)
@@ -327,17 +270,15 @@ void Rame::start_move( std::vector<Rame>& RAMES,Ligne & LIGNE, bool& Run)
 				precedente_station = (i+1)%LIGNE.quais.size();
 				myMutex.lock();
 				angle = 0.0;
-				position = PointDest;//remet le tram à la position de la station pour éviter des erreurs de calculs futurs
+				position = PointDest;//remet la rame à la position de la station pour éviter des erreurs de calculs futurs
 				myMutex.unlock();
 			}
-			sf::sleep(sf::milliseconds(400));
 		}
-		else
+		else//ARRET D'URGENCE ACTIF
 		{
 			
 			sf::sleep(sf::seconds(1));
-			//cout <<"LE "<<get_id()<<" "<< dist_to_next(RAMES, LIGNE) << endl; 
-			if ((emergency_stop == 1) && (dist_to_next(RAMES, LIGNE) > 500))
+			if ((emergency_stop == 1) && (dist_to_next(RAMES, LIGNE) > 550))
 			{
 				emergency_stop = 0;//retour à la normale
 			}
@@ -352,64 +293,6 @@ void Rame::move(sf::Time wait_time) {
 
 	std::lock_guard<std::mutex> locked(myMutex);
 
-	position.x += speed *direction.x* wait_time.asSeconds();	//speed*(double)(cos(angle))*wait_time.asSeconds();
-	position.y += speed * direction.y * wait_time.asSeconds();	//speed*(double)(sin(angle)) * wait_time.asSeconds();
+	position.x += speed *direction.x* wait_time.asSeconds();	//deplacement selon le vecteur unitaire direction
+	position.y += speed * direction.y * wait_time.asSeconds();	
 }
-
-
-
-																	//QUAI
-
-
-//classe qui gère les quais et nouveaux passagers
-class Quai
-{
-public:
-	int ligne;
-	Quai(int line) :ligne(line) {}
-
-	int nb_personnes_sur_quai = 0;
-	void set_nb_pers(int& nb) { nb_personnes_sur_quai = nb; }
-	int get_nb_pers() { return nb_personnes_sur_quai; }
-};
-
-
-																	//SYSTEM
-
-//ancien code plus utilisé
-class System
-{
-
-private:
-	
-public:
-
-	vector<Station> LIGNE;
-	vector<Rame> Engins;
-
-
-	System(string file_name = "bibliotheque_nom_stat.txt", int size=0, sf::Texture text_ram= sf::Texture(), sf::Texture text_stat= sf::Texture()) :LIGNE(size) { //organisé ainsi: Republique/Beaux/arts Lille/Flandres ...
-		ifstream Name_Stat(file_name);
-		for (int i = 0;i < LIGNE.size();++i)
-		{
-			string Name;
-			if (Name_Stat.peek()!='\n'&&!Name_Stat.eof()) { Name_Stat >> Name; }
-			else { Name = "Inconnu";}
-			modif_string(Name,'/',' ');
-			LIGNE.push_back(Station(i+1,Name,calcul_position_stat(i),sf::Sprite(text_stat)));
-
-		}
-	}
-	
-	//void print();
-};
-
-//void System::print()
-//{
-//	cout << "Le metro comprend:\n\n";
-//	int a = 6;
-//	for (auto i = LIGNE.begin();i != LIGNE.end();++i)
-//	{
-//		cout << "La station numero" << (*i).get_id() << " a pour nom " << (*i).get_name() << "\n\n";
-//	}
-//}  
